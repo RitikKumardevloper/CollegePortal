cfc073c25cbdca867f6739ecb68aaf0db9cbe5df
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-05-07 16:23:03 +0530

    fixed package issue

--- src/pages/Students/AllStudents.jsx
+++ src/pages/Students/AllStudents.jsx
@@ -5,6 +5,9 @@ import OverlayModal from "../../components/OverlayModal";
 import AddStudent from "./Addstudent"; // You can replace this with your form
 import breadcrumbs from "../../components/Breadcrumbs";
 import Breadcrumbs from "../../components/Breadcrumbs";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const AllStudents = () => {
   const [students, setStudents] = useState([]);
